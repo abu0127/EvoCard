6073854402be1b13cf0e213390299b6cd0b51489
abu0127 <abubakrtolipov0@gmail.com>
2025-05-30 01:28:31 +0500

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/taklifnoma-sayti/',
+  base: '/EvoCard/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
